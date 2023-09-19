ff0619217fd7e40210c3c17feb67d4c34a00f744
unknown <3laamohamed>
2023-09-19 11:45:40 +0300

    update

--- app/Models/Sub_group.php
+++ app/Models/Sub_group.php
@@ -14,17 +14,17 @@ class Sub_group extends Model
     protected $hidden = ['created_at','updated_at'];
     public function items()
     {
-        return $this->hasMany(Item:class,'sub_group_id','id');
+        return $this->hasMany(Item::class,'sub_group_id','id');
     }
 
     public function Group()
     {
-        return $this->belongsTo(Group:class,'group_id','id');
+        return $this->belongsTo(Group::class,'group_id','id');
     }
 
     public function Details()
     {
-        return $this->belongsToMany(Details:class,'details_sub_group','sub_id','detail_id')
+        return $this->belongsToMany(Details::class,'details_sub_group','sub_id','detail_id')
             ->withPivot(['price','section','max']);
     }
 }
