0d3ca2dc663399fd47e381fd31e1f69682e24fe7
hishamatf55 <hishamatf50@gmail.com>
2024-07-09 02:50:40 +0300

    added components to store crud

--- resources/views/includes/stock/Stock_Ajax/stores.blade.php
+++ resources/views/includes/stock/Stock_Ajax/stores.blade.php
@@ -1,79 +1,133 @@
 @include('includes.stock.Stock_Ajax.public_function')
 <script>
-    let id = $('#store_id');
-    let name = $('#store_name');
-    let phone = $('#store_phone');
-    let address = $('#store_address');
-    let tbody = $('.table-data tbody');
-    let branch = 0;
+    $(document).ready(function() {
+        $('.stores_btns').on('click', function() {
+            // let storeId = $(this).data('id');
+            let action = $(this).data('action');
+            let JsonMeta = [];
+            let id = $('#store_id');
+            let name = $('#store_name');
+            let phone = $('#store_phone');
+            let address = $('#store_address');
+            let branch = 0;
+            let methodChecks = $('input[name="storage_method"]:checked');
+            let storages = [];
+            let meta = [];
+            let tbody = $('.table-data tbody');
+            switch (action) {
+                case 'create':
+                    // validateData(name,phone,address,storages)
+                    methodChecks.each(function() {
+                        let type = $(this).val()
+                        let trMethod = $(this).parents('tr');
+                        let unit = trMethod.find('.unit').val();
+                        let capacity = trMethod.find('input[name="capacity"]').val();
 
-    $('#save_store').on('click', function() {
-        let methodChecks = $('input[name="storage_method"]:checked');
-        let storages = [];
-        methodChecks.each(function() {
-            let type = $(this).val()
-            let trMethod = $(this).parents('tr');
-            let unit = trMethod.find('.unit').val();
-            let capacity = trMethod.find('input[name="capacity"]').val();
+                        if (unit != null && capacity !== '') {
+                            storages.push({
+                                type: type,
+                                unit: unit,
+                                capacity: capacity
+                            });
+                        }
+                    });
+                    meta = {
+                        name: name,
+                        phone: phone,
+                        address: address,
+                    };
+                    validateForm(meta)
+                    break;
+                case 'update':
+                    break;
+                case 'view':
 
-            if (unit != null && capacity !== '') {
-                storages.push({
-                    type: type,
-                    unit: unit,
-                    capacity: capacity
+                    break;
+                case 'delete':
+                    break;
+                default:
+                    console.error('Unknown action:', action);
+                    return;
+            }
+
+            if (validateForm(meta)) {
+                JsonMeta = collectJsonMeta(action, meta)
+                JsonMeta.body.storages = storages
+                // callApi(JsonMeta);
+                $.ajax({
+                    url: JsonMeta.endPoint,
+                    method: JsonMeta.method,
+                    DataType: 'json',
+                    data: JsonMeta.body,
+                    success: function(data) {
+                        switch (action) {
+                            case 'create':
+                                if (data.status == 'true') {
+                                    let html = '';
+                                    tbody.empty();
+                                    data.stores.forEach(store => {
+                                        html += `<tr>
+                    <th>${store.id}</th>
+                    <td>${store.name || '-'}</td>
+                    <td>${store.phone || '-'}</td>
+                    <td>${store.address || '-'}</td>
+
+                     <td>
+                    <button title="تعديل" class="btn btn-success stores_btns"
+                        data-id="${ store.id }"data-action="edit">
+
+                        <i class="far fa-edit"></i>
+                    </button>
+
+                    <button title="عرض" data-id="${ store.id }" data-action="view"
+                        class="btn btn-primary stores_btns">
+
+                        <i class="fa fa-eye" aria-hidden="true"></i>
+
+                    </button>
+                    <button class="btn btn-danger stores_btns" data-action="delete"
+                        data-id="${ store.id }">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                               </td>
+                    </tr>`;
+                                    })
+                                    tbody.html(html);
+                                    id.val(data.new_store);
+                                    name.val('');
+                                    phone.val('');
+                                    address.val('');
+                                    $('input[type="checkbox"]').each(function() {
+                                        $(this).prop('checked', false)
+                                    })
+                                    $('.unit').each(function() {
+                                        $(this).find(' option:first-child')
+                                            .prop('selected', true)
+                                    })
+                                    $('input[name="capacity"]').each(function() {
+                                        $(this).val('')
+                                    })
+
+                                    successMsg(data.msg);
+                                }
+                            default:
+                                console.error('Unknown action:', action);
+                                return;
+                        }
+                    },
+                    error: function(reject) {
+                        let response = $.parseJSON(reject.responseText);
+                        $.each(response.errors, function(key, val) {
+                            errorMsg(val[0])
+                        });
+                    }
                 });
             }
-        });
 
-        $.ajax({
-            url: "{{ route('save.store') }}",
-            method: 'post',
-            data: {
-                _token,
-                name: name.val(),
-                phone: phone.val(),
-                address: address.val(),
-                storages: storages,
-            },
-            success: function(data) {
-                if (data.status == 'true') {
-                    let html = '';
-                    tbody.empty();
-                    data.stores.forEach(store => {
-                        html += `<tr>
-                            <th>${store.id}</th>
-                            <td>${store.name || '-'}</td>
-                            <td>${store.phone || '-'}</td>
-                            <td>${store.address || '-'}</td>
-                        </tr>`;
-                    })
-                    tbody.html(html);
-                    id.val(data.new_store);
-                    name.val('');
-                    phone.val('');
-                    address.val('');
-                    $('input[type="checkbox"]').each(function() {
-                        $(this).prop('checked', false)
-                    })
-                    $('.unit').each(function() {
-                        $(this).find(' option:first-child').prop('selected', true)
-                    })
-                    $('input[name="capacity"]').each(function() {
-                        $(this).val('')
-                    })
-
-                    successMsg(data.msg);
-                    checkForm();
-                }
-            },
-            error: function(reject) {
-                let response = $.parseJSON(reject.responseText);
-                $.each(response.errors, function(key, val) {
-                    errorMsg(val[0])
-                });
-            }
         });
     });
+
+
 
     $('#store_name').on('keyup', function() {
         let query = $(this).val()
@@ -130,12 +184,12 @@
             url: "{{ route('update.store') }}",
             method: 'post',
             data: {
-                _token,
+                _token: "{{ csrf_token() }}",
                 id: id.val(),
                 name: name.val(),
                 phone: phone.val(),
                 address: address.val(),
-                storages,
+                // storages,
             },
             success: function(data) {
                 if (data.status == 'true') {
@@ -143,11 +197,11 @@
                     tbody.empty();
                     data.stores.forEach(store => {
                         html += `<tr>
-                            <th>${store.id}</th>
-                            <td>${store.name || '-'}</td>
-                            <td>${store.phone || '-'}</td>
-                            <td>${store.address || '-'}</td>
-                        </tr>`;
+                        <th>${store.id}</th>
+                        <td>${store.name || '-'}</td>
+                        <td>${store.phone || '-'}</td>
+                        <td>${store.address || '-'}</td>
+                    </tr>`;
                     })
                     tbody.html(html);
                     id.val(data.new_store);
@@ -202,8 +256,8 @@
                         "_token": "{{ csrf_token() }}"
                     },
                     success: function(response) {
-                       console.log(response)
-                       return false;
+                        console.log(response)
+                        return false;
                         swal({
                             title: 'Deleted!',
                             text: 'Your store has been deleted successfully.',
